c882a0400c8771df7397709e712210c67fe2fc91
Yash Chittora <yashchittora.code@gmail.com>
2023-04-10 20:03:09 +0530

    Add Day1

---
+++ Day1.py
@@ -0,0 +1,17 @@
+# Print Statements
+
+print("Aloo")
+print("New line goes here \nTab Here \t Hello")
+print(r"New line \n Tab \t")
+print("Mango","Potato","Carrot", sep = " * ")
+
+print("Python",end = "\n")
+print("Python",end = " -- ")
+print("Python",end = " ^_^ ")
+print()
+a,b = 10,20
+print("Hiii {}".format(a))
+print("Hiii {} {}".format(a,b))
+print("Hiii {1} {0}".format(a,b))
+print(f"Hiii {a} {b}")
+print("Hiii {a} {b}")
