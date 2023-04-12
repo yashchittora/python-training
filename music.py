8c7e4d1824c576c360c0c741565f83dadf76aeef
Yash Chittora <yashchittora.code@gmail.com>
2023-04-12 14:59:59 +0530

    Add Music Day 3

---
+++ music.py
@@ -0,0 +1,24 @@
+from pygame import mixer
+
+mixer.init()
+
+mixer.music.load("Tum se Hi.mp3")
+mixer.music.set_volume(10)
+
+mixer.music.play()
+
+while(True):
+    print("P to pause , R to Resume , E to exit")
+    c = input("Enter choice : ")
+    if c.lower() == 'p':
+        mixer.music.pause()
+    
+    elif c.lower() == 'r':
+        mixer.music.unpause()
+    
+    elif c.lower() == 'ee':
+        mixer.music.stop()
+    
+
+
+
